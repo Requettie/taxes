6b92810d0014880e780738419510e430f0f9e90a
Requette <79616448+Requettie@users.noreply.github.com>
2021-05-21 22:38:28 -0700

    Add files via upload

---
+++ taxes/script.js
@@ -0,0 +1,18 @@
+switch(prompt("What would you like to buy?")){
+    case "candy":
+        console.log("30"*"1.16");
+        break;
+    case "apples":
+    console.log("15"*"1.16");
+    case "crackers":
+        console.log("50"*"1.16");
+        break;
+    case "Coca-Cola":
+        console.log("10"*"1.16");
+        break;
+    default:
+        console.log("Buy something from me!");
+        break;
+    }
+
+   
